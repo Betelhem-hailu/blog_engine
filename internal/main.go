7955f293dedbdb1e3a8874e1722e273110b2dc94
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-07-29 11:49:59 +0300

    build: implement basic routing

--- internal/main.go
+++ internal/main.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    
+	r := gin.Default()
+	// r.LoadHTMLGlob("templates/*")
+	// r.GET("/index", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	// 		"title": "Main website",
+	// 	})
+	// })
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Home Page!",
+		})
+	})
+	r.GET("/about", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "This is the About Page.",
+        })
+    })
+	r.GET("/user/:name", func(c *gin.Context) {
+        name := c.Param("name")
+        c.JSON(http.StatusOK, gin.H{
+            "message": "Hello " + name,
+        })
+    })
+	r.Run()
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,31 @@
 package routes
+
+import (
+    "go-gin-blog/controllers"
+    "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+)
+
+func SetupRoutes(r *gin.Engine, db *gorm.DB) {
+    pc := &controllers.PostController{DB: db}
+
+    r.GET("/posts", pc.GetPosts)
+    r.POST("/posts", pc.CreatePost)
+	r.PUT("/posts/:id", pc.UpdatePost)
+	r.DELETE("/posts/:id", pc.DeletePost)
+
+	cc := &controllers.CommentController{DB: db}
+
+	r.GET("/comments", cc.GetComments)
+    r.POST("/comments", cc.CreateComment)
+	r.PUT("/comments/:id", cc.UpdateComment)
+	r.DELETE("/comments/:id", cc.DeleteComment)
+
+	uc := &controllers.UserController{DB: db}
+
+	r.GET("/user", uc.GetUser)
+	r.GET("/logout", uc.GetUser)
+    r.POST("/register", uc.CreateUser)
+    r.POST("/login", uc.LoginUser)
+	r.PUT("/user/:id", uc.UpdateUser)
+}
