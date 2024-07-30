448706cbab2b4a7a494e4ca6077530b1cb00043b
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-07-30 08:14:16 +0300

    build:models for posts, users & comments

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -1,2 +1,14 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Comment struct {
+    gorm.Model
+	Id         int    `json:"id"`
+    Name   string `json:"name"`
+    Message string `json:"message"`
+	Created Time
+	Updated Time
+}
--- internal/models/post.go
+++ internal/models/post.go
@@ -1,3 +1,22 @@
 package models
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+    gorm.Model
+	Id         int    `json:"id"`
+    Title   string `json:"title"`
+    Content string `json:"content"`
+	Created Time
+	Updated Time
+}
+
+
+type Time struct {
+	T time.Time
+}
 
